7444213745eee4b230b9e8a3e405a1eb7c71a417
transformersinnovationhub <transformersinnovationhub@gmail.com>
2025-11-02 15:29:06 +0100

    Add files via upload

---
+++ citizen/script.js
@@ -0,0 +1,4 @@
+const welcome_message=document.querySelector(".welcome-message")
+const name="Ms. Loma";
+welcome_message.innerText="Welcome, " +name;
+
--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 const ident=document.querySelectorAll(".ident div");
 
+const butlink=document.querySelector(".butlink");
 
+// ident.forEach(iden => {
+//       iden.addEventListener('click', () => {
+//         ident.forEach(id => id.classList.remove('active'));
+//         contents.forEach(i => i.classList.remove('active'));
+//         iden.classList.add('active');
+//         document.getElementById(iden.dataset.target).classList.add('active');
+//       });
+//     });
 
-ident.forEach(iden => {
-      iden.addEventListener('click', () => {
-        ident.forEach(id => id.classList.remove('active'));
-        contents.forEach(i => i.classList.remove('active'));
-        iden.classList.add('active');
-        document.getElementById(iden.dataset.target).classList.add('active');
-      });
-    });
+ident.forEach(identi => {
+  identi.addEventListener('click', ()=>{
+    ident.forEach(id=>id.classList.remove('active'));
+    identi.classList.add('active');
+    document.getElementById(identi.dataset.target).classList.add('active');
+  })
+})
+
+ident[0].addEventListener('click', ()=>{
+  butlink.innerHTML=`<a href="admin\\admin.html">Verify & Sign up</a>`;
+});
+ident[1].addEventListener('click', ()=>{
+  butlink.innerHTML=`<a href="citizen\\home.html">Verify & Sign up</a>`;
+});
+ident[2].addEventListener('click', ()=>{
+  butlink.innerHTML=`<a href="admin\\admin.html">Verify & Sign up</a>`;
+});
